027e44de64eda996546b734b26b7b8f78ee703b7
mephirious <hardwarerump04@gmail.com>
2025-02-17 07:51:23 +0500

    fix: footer styles fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import SupportPage from './pages/SupportPage/SupportPage';
 
 function App() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(verifyAuth());
   }, [dispatch]);
@@ -26,36 +27,36 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductSingle />} />
-          <Route path="/category/:category" element={<CategoryProduct />} />
-          <Route path="/brands/:brand" element={<BrandProductPage />} />
-          <Route path="/types/:type" element={<TypeProductPage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/search/:searchTerm" element={<Search />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/compare" element={<LaptopComparison />} />
-          <Route path="/blog/:id" element={<BlogSingle />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="/terms-of-service" element={<TermsOfServicePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/support" element={<SupportPage />} />
-
-
-          <Route
-            path="/admin/edit"
-            element={
-              <ProtectedRoute adminOnly={true}>
-                <AdminEditPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <div className="content">
+          <Header />
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductSingle />} />
+            <Route path="/category/:category" element={<CategoryProduct />} />
+            <Route path="/brands/:brand" element={<BrandProductPage />} />
+            <Route path="/types/:type" element={<TypeProductPage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/search/:searchTerm" element={<Search />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/compare" element={<LaptopComparison />} />
+            <Route path="/blog/:id" element={<BlogSingle />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/support" element={<SupportPage />} />
+            <Route
+              path="/admin/edit"
+              element={
+                <ProtectedRoute adminOnly={true}>
+                  <AdminEditPage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
